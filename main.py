8d640eb6826359163eda2b59a2cae62573efaad3
AsimFakhi <asimfakhi11@gmail.com>
2026-05-14 22:46:09 +0530

    FastAPI - Understanding Query Parameters

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
+
+user_list = [
+   "Asim",
+   "Saud",
+   "Atiya"
+]
+
 
 @app.get('/')
 async def read_root():
     return {'message': 'Bismillah!!'}
+
+@app.get('/greet/{username}')
+async def greet(username:str):
+    return {"message": f"Hello {username}"}
+
+@app.get('/greet/')
+async def greet(username:Optional[str]='User'):
+    return {"message": f"Hello {username}"}
+
+@app.get('/search')
+async def search_for_user(username:str):
+        if username in user_list:
+            return {"message": f"Users details fetched for {username}"}
+        else:
+            return {"message": "User not found."}
