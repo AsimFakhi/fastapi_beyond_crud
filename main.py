2e6a258c1c402f70158a1b87bb01020961a69955
AsimFakhi <asimfakhi11@gmail.com>
2026-05-15 12:38:18 +0530

    practiced request body and fetching headers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -8,6 +9,10 @@ user_list = [
    "Saud",
    "Atiya"
 ]
+users = []
+class UserSchema(BaseModel):
+     username:str
+     email:str
 
 
 @app.get('/')
@@ -28,3 +33,31 @@ async def search_for_user(username:str):
             return {"message": f"Users details fetched for {username}"}
         else:
             return {"message": "User not found."}
+    
+@app.post('/create_user')
+async def create_user(user_data:UserSchema):
+    new_user = {
+                'username' : user_data.username,
+                'email' : user_data.email
+             }
+    users.append(new_user)
+    return {'message': 'Users created successfully.','user':new_user}
+
+@app.get('/get_headers')
+async def get_all_request_headers(
+     user_agent: Optional[str] = Header(None),
+     accept_encoding: Optional[str] = Header(None),
+     referer: Optional[str] = Header(None),
+     connection: Optional[str] = Header(None),
+     accept_language: Optional[str] = Header(None),
+     host: Optional[str] = Header(None),
+):
+     request_headers={}
+     request_headers["user_agent"]=user_agent
+     request_headers["accept_encoding"]=accept_encoding
+     request_headers["referer"]=referer
+     request_headers["connection"]=connection
+     request_headers["accept_language"]=accept_language
+     request_headers["host"]=host
+
+     return request_headers
