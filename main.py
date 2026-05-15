4af9386c325ad31acde41fd78b912d1cd53e5111
Asim Fakhi <asimfakhi11@gmail.com>
2026-05-15 20:51:13 +0530

    Completed Books CRUD REST API

--- main.py
+++ main.py
@@ -1,63 +1,111 @@
-from fastapi import FastAPI, Header
-from typing import Optional
+from fastapi import FastAPI, Header, status
+from typing import Optional, List
 from pydantic import BaseModel
+from fastapi import HTTPException
 
 app = FastAPI()
 
-user_list = [
-   "Asim",
-   "Saud",
-   "Atiya"
+books = [
+    {
+        "id": 1,
+        "title": "Think Python",
+        "author": "Allen B. Downey",
+        "publisher": "O'Reilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 1234,
+        "language": "English",
+    },
+    {
+        "id": 2,
+        "title": "Django By Example",
+        "author": "Antonio Mele",
+        "publisher": "Packt Publishing Ltd",
+        "published_date": "2022-01-19",
+        "page_count": 1023,
+        "language": "English",
+    },
+    {
+        "id": 3,
+        "title": "The web socket handbook",
+        "author": "Alex Diaconu",
+        "publisher": "Xinyu Wang",
+        "published_date": "2021-01-01",
+        "page_count": 3677,
+        "language": "English",
+    },
+    {
+        "id": 4,
+        "title": "Head first Javascript",
+        "author": "Hellen Smith",
+        "publisher": "Oreilly Media",
+        "published_date": "2021-01-01",
+        "page_count": 540,
+        "language": "English",
+    },
+    {
+        "id": 5,
+        "title": "Algorithms and Data Structures In Python",
+        "author": "Kent Lee",
+        "publisher": "Springer, Inc",
+        "published_date": "2021-01-01",
+        "page_count": 9282,
+        "language": "English",
+    },
+    {
+        "id": 6,
+        "title": "Head First HTML5 Programming",
+        "author": "Eric T Freeman",
+        "publisher": "O'Reilly Media",
+        "published_date": "2011-21-01",
+        "page_count": 3006,
+        "language": "English",
+    },
 ]
-users = []
-class UserSchema(BaseModel):
-     username:str
-     email:str
 
+class Book(BaseModel):
+    id: int
+    title: str
+    author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
 
-@app.get('/')
-async def read_root():
-    return {'message': 'Bismillah!!'}
+class BookUpdateModel(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    page_count: int
+    language: str
 
-@app.get('/greet/{username}')
-async def greet(username:str):
-    return {"message": f"Hello {username}"}
+@app.get("/books", response_model=List[Book])
+async def get_all_books():
+    return books
 
-@app.get('/greet/')
-async def greet(username:Optional[str]='User'):
-    return {"message": f"Hello {username}"}
+@app.get("/book/{book_id}")
+async def get_book(book_id: int) -> dict:
+    for book in books:
+        if book["id"] == book_id:
+            return book
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found.")
 
-@app.get('/search')
-async def search_for_user(username:str):
-        if username in user_list:
-            return {"message": f"Users details fetched for {username}"}
-        else:
-            return {"message": "User not found."}
-    
-@app.post('/create_user')
-async def create_user(user_data:UserSchema):
-    new_user = {
-                'username' : user_data.username,
-                'email' : user_data.email
-             }
-    users.append(new_user)
-    return {'message': 'Users created successfully.','user':new_user}
+@app.patch("/book/{book_id}")
+async def update_book(book_id: int, book_update_data:BookUpdateModel):
+    for book in books:
+        if book["id"] == book_id:
+            book["title"] = book_update_data.title
+            book["publisher"] = book_update_data.publisher
+            book["page_count"] = book_update_data.page_count
+            book["language"] = book_update_data.language
 
-@app.get('/get_headers')
-async def get_all_request_headers(
-     user_agent: Optional[str] = Header(None),
-     accept_encoding: Optional[str] = Header(None),
-     referer: Optional[str] = Header(None),
-     connection: Optional[str] = Header(None),
-     accept_language: Optional[str] = Header(None),
-     host: Optional[str] = Header(None),
-):
-     request_headers={}
-     request_headers["user_agent"]=user_agent
-     request_headers["accept_encoding"]=accept_encoding
-     request_headers["referer"]=referer
-     request_headers["connection"]=connection
-     request_headers["accept_language"]=accept_language
-     request_headers["host"]=host
+            return book
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found.")
 
-     return request_headers
+@app.delete("/book/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id:int):
+    for book in books:
+        if book["id"] == book_id:
+            books.remove(book)
+            return {}
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found.")
