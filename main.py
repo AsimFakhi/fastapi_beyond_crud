ddf0b10f1981251c7c6b6ab3ea7c5b04dbf23a67
Asim Fakhi <asimfakhi11@gmail.com>
2026-05-15 23:11:09 +0530

    Restructure project using router

--- main.py
+++ main.py
@@ -1,111 +1,15 @@
-from fastapi import FastAPI, Header, status
-from typing import Optional, List
-from pydantic import BaseModel
-from fastapi import HTTPException
+from fastapi import FastAPI
+from src.books.routes import book_router
 
-app = FastAPI()
+version = 'v1'
 
-books = [
-    {
-        "id": 1,
-        "title": "Think Python",
-        "author": "Allen B. Downey",
-        "publisher": "O'Reilly Media",
-        "published_date": "2021-01-01",
-        "page_count": 1234,
-        "language": "English",
-    },
-    {
-        "id": 2,
-        "title": "Django By Example",
-        "author": "Antonio Mele",
-        "publisher": "Packt Publishing Ltd",
-        "published_date": "2022-01-19",
-        "page_count": 1023,
-        "language": "English",
-    },
-    {
-        "id": 3,
-        "title": "The web socket handbook",
-        "author": "Alex Diaconu",
-        "publisher": "Xinyu Wang",
-        "published_date": "2021-01-01",
-        "page_count": 3677,
-        "language": "English",
-    },
-    {
-        "id": 4,
-        "title": "Head first Javascript",
-        "author": "Hellen Smith",
-        "publisher": "Oreilly Media",
-        "published_date": "2021-01-01",
-        "page_count": 540,
-        "language": "English",
-    },
-    {
-        "id": 5,
-        "title": "Algorithms and Data Structures In Python",
-        "author": "Kent Lee",
-        "publisher": "Springer, Inc",
-        "published_date": "2021-01-01",
-        "page_count": 9282,
-        "language": "English",
-    },
-    {
-        "id": 6,
-        "title": "Head First HTML5 Programming",
-        "author": "Eric T Freeman",
-        "publisher": "O'Reilly Media",
-        "published_date": "2011-21-01",
-        "page_count": 3006,
-        "language": "English",
-    },
-]
+app = FastAPI(
+    title='FastAPI beyond CRUD',
+    description='A RESTful API for a book review web service',
+    version=version
+)
 
-class Book(BaseModel):
-    id: int
-    title: str
-    author: str
-    publisher: str
-    published_date: str
-    page_count: int
-    language: str
+app.include_router(book_router, prefix=f'/api/{version}/books', tags=['books'])
 
-class BookUpdateModel(BaseModel):
-    title: str
-    author: str
-    publisher: str
-    page_count: int
-    language: str
 
-@app.get("/books", response_model=List[Book])
-async def get_all_books():
-    return books
 
-@app.get("/book/{book_id}")
-async def get_book(book_id: int) -> dict:
-    for book in books:
-        if book["id"] == book_id:
-            return book
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found.")
-
-@app.patch("/book/{book_id}")
-async def update_book(book_id: int, book_update_data:BookUpdateModel):
-    for book in books:
-        if book["id"] == book_id:
-            book["title"] = book_update_data.title
-            book["publisher"] = book_update_data.publisher
-            book["page_count"] = book_update_data.page_count
-            book["language"] = book_update_data.language
-
-            return book
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found.")
-
-@app.delete("/book/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_book(book_id:int):
-    for book in books:
-        if book["id"] == book_id:
-            books.remove(book)
-            return {}
-
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found.")
